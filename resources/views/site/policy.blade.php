797a8b868352b23cb84a3771e499e7e2d7ba3d39
thang197 <thangnguyennvt1410@gmail.com>
2025-11-07 16:50:35 +0700

    F

--- resources/views/site/policy.blade.php
+++ resources/views/site/policy.blade.php
@@ -4,7 +4,7 @@
 @section('image'){{@$config->image->path ?? ''}}@endsection
 
 @section('css')
-    <link type="text/css" rel="stylesheet" href="/site/assets/styles/editor-content.css?v=2.0">
+    <link type="text/css" rel="stylesheet" href="/site/assets/styles/editor-content.css?v=3.0">
 
 @endsection
 
